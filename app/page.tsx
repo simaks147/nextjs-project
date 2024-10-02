472e7cf2366bd2027139320c00c90bc388622c21
maks lobarev <maksim147@yandex.ru>
2024-10-02 18:44:22 +0300

    main header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <h1>Home
-      <Button variant='outline'>Корзина</Button>
-    </h1>
-  );
+  return <h1>Главная</h1>;
 }
