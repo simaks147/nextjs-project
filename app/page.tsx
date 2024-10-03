382289ace7806e119ab2539928dd2b950c37f350
maks lobarev <maksim147@yandex.ru>
2024-10-03 16:31:28 +0300

    sticky top bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { Container, Title } from "@/components/shared";
-import { Categories } from "@/components/shared";
+import { Container, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return <>
     <Container className="mt-10">
       <Title text="Все пиццы" size="lg" className="font-extrabold" />
-      <Categories />
     </Container>
+    <TopBar />
   </>;
 }
