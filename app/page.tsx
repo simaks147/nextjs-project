c596637b333d27dfab41d1c03cd5a330dbc443ee
maks lobarev <maksim147@yandex.ru>
2024-10-03 12:40:50 +0300

    menu of categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Container, Title } from "@/components/shared";
+import { Categories } from "@/components/shared";
+
 export default function Home() {
-  return <h1>Главная</h1>;
+  return <>
+    <Container className="mt-10">
+      <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      <Categories />
+    </Container>
+  </>;
 }
